ca359649d35d6fc3cd90cacc335c3d0b9a0fc321
Roy Teeuwen <roy@orbinson.be>
2023-01-04 18:47:28 +0100

    Start using the bnd maven plugin to create a manifest file

--- api/src/main/groovy/be/orbinson/aem/groovy/console/configuration/package-info.groovy
+++ api/src/main/groovy/be/orbinson/aem/groovy/console/configuration/package-info.groovy
@@ -1,3 +1,3 @@
 @org.osgi.annotation.bundle.Export
-@org.osgi.annotation.versioning.Version("1.0.0")
+@org.osgi.annotation.versioning.Version("18.0.0")
 package be.orbinson.aem.groovy.console.configuration;
