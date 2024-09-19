ba3eb2cf10db56ac65f894a555cf714202218843
Roy Teeuwen <roy@orbinson.be>
2024-09-19 20:57:37 +0200

    Debug request and response headers

--- it.tests/src/test/java/be/orbinson/aem/groovy/console/it/GroovyConsoleServiceIT.java
+++ it.tests/src/test/java/be/orbinson/aem/groovy/console/it/GroovyConsoleServiceIT.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonSyntaxException;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -14,6 +15,8 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -26,10 +29,30 @@ public class GroovyConsoleServiceIT {
 
     @Test
     void testScriptReturnsResult() throws Exception {
+        printHealth();
+        System.out.println("Starting test at " + Instant.now());
+        Thread.sleep(3000); // TODO, fix to use system health
+        System.out.println("Executing script  at " + Instant.now());
+        printHealth();
+
+
         String script = "print 'test'";
         Map response = executeScript(script);
+        System.out.println("Got response script  at " + Instant.now());
+
         assertNotNull(response, "Could not get response from API");
         assertEquals("test", response.get("output"));
+    }
+
+    private void printHealth() throws IOException {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            HttpGet printHealth = new HttpGet("http://localhost:" + SLING_PORT + "/system/health.json");
+            printHealth.addHeader("Authorization", "Basic " + Base64.encodeBase64String("admin:admin".getBytes(StandardCharsets.UTF_8)));
+            try (CloseableHttpResponse response = httpclient.execute(printHealth)) {
+                String body = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+                System.out.println(body);
+            }
+        }
     }
 
     private static Map executeScript(String script) throws IOException {
@@ -39,13 +62,25 @@ public class GroovyConsoleServiceIT {
             basicNameValuePairs.add(new BasicNameValuePair("script", script));
             executeScript.setEntity(new UrlEncodedFormEntity(basicNameValuePairs, StandardCharsets.UTF_8));
             executeScript.addHeader("Authorization", "Basic " + Base64.encodeBase64String("admin:admin".getBytes(StandardCharsets.UTF_8)));
+
+            System.out.println("Request headers");
+            Arrays.stream(executeScript.getAllHeaders()).forEach(header ->
+                    System.out.println(header.getName() + ": " + header.getValue())
+            );
             try (CloseableHttpResponse response = httpclient.execute(executeScript)) {
+                System.out.println("Response headers");
+
+                Arrays.stream(response.getAllHeaders()).forEach(header ->
+                        System.out.println(header.getName() + ": " + header.getValue())
+                );
+                System.out.println(response.getStatusLine());
+
                 String body = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
                 try {
                     return new Gson().fromJson(body, Map.class);
                 } catch (JsonSyntaxException e) {
                     System.out.println("Could not parse body from JSON: " + e.getMessage());
-                    System.out.println(body);
+                   // System.out.println(body);
                     return null;
                 }
             }
